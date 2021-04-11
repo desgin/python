5bd099fdb3baa0e1b22038e218e082efcdc7b6cd
Devi <devi.frontenddev@gmail.com>
2021-04-11 18:51:47 -0500

    adding lambda experession

---
+++ try_function.py
@@ -0,0 +1,12 @@
+def max(int1, int2):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+print("Enter Two Integers\n\n")
+a = int(input('Enter First Integer:'))
+b = int(input('Enter second Integer:'))
+
+print(max(a, b))
---
+++ try_lambda.py
@@ -0,0 +1,12 @@
+# lambda function
+
+ctof = lambda c: 9 / 5 * c + 32
+
+# input
+c = int(input("Enter Temp(C):"))
+
+# function call
+f = ctof(c)
+
+# print
+print("Temp(F) :", f)
