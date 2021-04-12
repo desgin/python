c846376419bf6f0eb80c3e71b1934b7c81af4dcd
Devi <devi.frontenddev@gmail.com>
2021-04-12 16:23:03 -0500

    adding the animals file

---
+++ animals.py
@@ -0,0 +1,30 @@
+class Mammals:
+
+    def print_method(self):
+        print("Mammals don't lay eggs")
+
+
+class Dogs(Mammals):
+
+    def print_method(self):
+        print("Dogs bark")
+
+
+class Cats(Mammals):
+
+    def print_method(self):
+        print("Cats hiss")
+
+
+def main():
+    print("In main method")
+    y = Mammals()
+    x = Dogs()
+    z = Cats()
+    y.print_method()
+    x.print_method()
+    z.print_method()
+
+
+if __name__ == "__main__":
+    main()
